e4a2e46b74bc3c5132e463480efbd89f70caf0b0
Bradley Smagacz <bsmagacz@truecar.com>
2021-03-01 15:07:46 -0800

    add images

--- src/utils/avatarUtils.tsx
+++ src/utils/avatarUtils.tsx
@@ -1,11 +1,23 @@
 import { ImageURISource } from "react-native";
+//@ts-expect-error
+import avatar0 from "./assets/avatar-0.png";
+//@ts-expect-error
+import avatar1 from "./assets/avatar-1.png";
+//@ts-expect-error
+import avatar2 from "./assets/avatar-2.png";
+//@ts-expect-error
+import avatar3 from "./assets/avatar-3.png";
+//@ts-expect-error
+import avatar4 from "./assets/avatar-4.png";
+//@ts-expect-error
+import avatar5 from "./assets/avatar-5.png";
 
-const avatar0 = require("../assets/avatar-0.png");
-const avatar1 = require("../assets/avatar-1.png");
-const avatar2 = require("../assets/avatar-2.png");
-const avatar3 = require("../assets/avatar-3.png");
-const avatar4 = require("../assets/avatar-4.png");
-const avatar5 = require("../assets/avatar-5.png");
+// const avatar0 = require("./assets/avatar-0.png");
+// const avatar1 = require("./assets/avatar-1.png");
+// const avatar2 = require("./assets/avatar-2.png");
+// const avatar3 = require("./assets/avatar-3.png");
+// const avatar4 = require("./assets/avatar-4.png");
+// const avatar5 = require("./assets/avatar-5.png");
 
 const avatars: { [key: string]: ImageURISource } = {
   "0": avatar0,
