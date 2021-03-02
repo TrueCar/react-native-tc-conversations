10817b92deb70a822501906b5a56c01b90eeaab8
Bradley Smagacz <bsmagacz@truecar.com>
2021-03-02 13:24:17 -0800

    change to one index

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,40 @@
-export * from './components/Conversations/ConversationCenter';
+/* eslint-disable */
+import React from "react";
+export type ConversationProps = {
+  bottomOffset?: number;
+  manualOfferBtnComponent?: (prospect: ProspectInfo) => React.ReactNode;
+  navBarRightComponent?: (prospect: ProspectInfo) => React.ReactNode;
+  onManualOfferPress?: () => void;
+  onNavBarPressLeft?: () => void;
+  onNavBarPressRight?: () => void;
+  onNavBarPressTitle?: () => void;
+  participantOptedOut?: boolean;
+  renderCustomAvatar?: () => React.ReactNode;
+  onUserMessagesView?: (prospectId: string) => void;
+  onCameraPressed?: () => void;
+  predefinedChatInput?: string;
+  renderUsernameOnMessage?: boolean;
+  tokenEndpoint: string;
+  prospectId: string;
+  webMode?: boolean;
+};
+export type ProspectInfo = {
+  id: string;
+  name: string;
+};
+export declare const Conversation: ({
+  bottomOffset,
+  manualOfferBtnComponent,
+  onManualOfferPress,
+  navBarRightComponent,
+  onNavBarPressLeft,
+  onNavBarPressRight,
+  onNavBarPressTitle,
+  participantOptedOut,
+  renderCustomAvatar,
+  predefinedChatInput,
+  onUserMessagesView,
+  onCameraPressed,
+  renderUsernameOnMessage,
+  webMode,
+}: ConversationProps) => JSX.Element;
